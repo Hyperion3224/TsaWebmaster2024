194bf905eb111b291de715f10fb09b49bfd95156
Aisihe <aarushabrol07@gmail.com>
2024-02-03 01:32:48 -0600

    fucing woiarghoiewrg

--- JavaScript/anim.js
+++ JavaScript/anim.js
@@ -22,10 +22,86 @@ const observer = new IntersectionObserver(
 )
 
 
-cards.forEach(card => {
-    observer.observe(card)
-})
 
+//--------------------------------------------------------
+
+const textrotated = document.querySelectorAll('.eep-info')
+console.log(textrotated)
+let counter3 = 0;
+const textObserver = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            counter3++;
+            console.log(counter2)
+            odd = entry.isIntersecting && counter3%2 === 0;
+            even = entry.isIntersecting && counter3%2 === 1;
+            if(counter3>0){
+                if(odd){
+                    entry.target.classList.add("show2")
+                    textObserver.unobserve(entry.target)
+                }else if(even){
+                    entry.target.classList.add("show")
+                    textObserver.unobserve(entry.target)
+                }
+            }
+            else if (counter3 == 0){
+                entry.target.classList.add("show")
+                textObserver.unobserve(entry.target)
+            }
+        } )
+    },
+    {
+        threshold: .5,
+    }
+)
+
+
+
+//--------------------------------------------------------
+
+const imagerotate = document.querySelectorAll('.eep-img')
+let counter2 = -1;
+const imgObserver = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            counter2++;
+            console.log(counter2)
+            odd = entry.isIntersecting && counter2%2 === 0;
+            even = entry.isIntersecting && counter2%2 === 1;
+            if(counter2>0){
+                if(odd){
+                    entry.target.classList.add("show2")
+                    imgObserver.unobserve(entry.target)
+                }else if(even){
+                    entry.target.classList.add("show")
+                    imgObserver.unobserve(entry.target)
+                }
+            }
+            else if (counter2 == 0){
+                entry.target.classList.add("show2")
+                imgObserver.unobserve(entry.target)
+            }
+            
+        } )
+    },
+    {
+        threshold: .5,
+    }
+)
+
+
+    cards.forEach(card => {
+        observer.observe(card)
+    })
+    imagerotate.forEach(img => {
+        imgObserver.observe(img)
+    })
+    textrotated.forEach(text => {
+        textObserver.observe(text)
+    })
+
+
+//--------------------------------------------------------
 var counter = -1;
 var positionArray = [0, 0];
 

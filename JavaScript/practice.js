09d090993d4ea5403c614496894df223cb8cf9fd
aengu78 <reimxnade@gmail.com>
2024-05-30 01:16:41 -0500

    added practices info

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -17,22 +17,23 @@ var titles = [
 ];
 
 var contents = [
-    "Air Source Heat Pumps",
-    "Recycling",
-    "Donation",
-    "Composting",
-    "Insulation",
-    "Diet"
+    "&esmp; Air source heat pumps (ASHPs) are versatile renewable energy systems for heating and cooling. To optimize their use, understand their operation, assess your property, consider efficiency and costs, choose a suitable ASHP, and ensure proper installation and maintenance. ASHPs contribute to environmental benefits by enhancing energy efficiency, lowering environmental impact, and more.",
+    "&esmp; Home recycling is an effective way to cut waste and environmental impact. Consider knowing local guidelines, separating recyclables, cleaning containers, buying recycled products, and educating others. Benefits include resource conservation, energy savings, less landfill waste, lower emissions, and job creation.",
+    "&esmp; Donating old belongings, instead of discarding them, promotes environmental sustainability. Try researching local donation centers, exploring specialty programs, using online platforms, and more. Increased donations reduce landfill waste, conserve resources, promote reuse and circularity, save energy, cut emissions, support local communities, and encourage responsible consumption.",
+    "&esmp; Composting is a rewarding and simple process. Numerous online guides and tips are readily available. It is an environmentally friendly waste management practice with benefits such as reduced landfill waste, erosion control, less need for chemical fertilizers and pesticides, water conservation, and a lower carbon footprint.",
+    "&esmp; Enhancing home insulation and energy efficiency benefits both comfort and the environment. Assess your home, select appropriate insulation, and install it professionally or DIY. This improves energy efficiency, reduces carbon footprint, conserves resources, addresses climate change, enhances indoor air quality, and promotes long-term sustainability.",
+    "&esmp; Reducing meat intake benefits the environment. Try exploring plant-based proteins, trying meat substitutes, adjusting recipes, gradually cutting meat consumption, and practicing mindful eating. Environmental gains include lower greenhouse gas emissions, efficient land use, water conservation, reduced deforestation, less pollution, energy savings, and biodiversity preservation."
 ];
 
 var disclaimers = [
-    "Air Source Heat Pumps",
-    "Recycling",
-    "Donation",
-    "Composting",
-    "Insulation",
-    "Diet"
+    "Note that performance and efficiency of air source heat pumps may vary based on climate conditions and specific installation factors.",
+    "Note that recycling processes and the acceptance of materials can vary by location, and not all items are recyclable in every area.",
+    "Note that donating items is subject to the acceptance policies of the receiving organization, and not all items may be suitable or accepted for donation.",
+    "Note that composting is subject to local regulations and guidelines, and not all materials are suitable for composting in every environment.",
+    "Note that insulation effectiveness can vary based on material type, installation quality, and environmental conditions.",
+    "NOte that reducing meat in your diet may have different health and nutritional impacts depending on individual dietary needs and should be approached with balanced nutritional planning."
 ];
+
 
 var index = 0;
 
